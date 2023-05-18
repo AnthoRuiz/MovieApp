bc7ef1223a36d8fbdd89b67bd175588b6cac655e
Anthony Ruiz <yobakaj@gmail.com>
2023-05-18 17:50:07 -0400

    First commit, Movie app, created services to pull data from api

---
+++ App.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { getPopularMovies } from './services/services';
+
+const App = () => {
+  const [movie, setMovie] = useState('');
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getPopularMovies()
+      .then(movies => {
+        setMovie(movies[0]);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Movie Name: {movie.original_title}</Text>
+      <Text>Release Date: {movie.release_date}</Text>
+      {error && <Text style={{ color: 'red' }}>Error in the Server</Text>}
+    </View>
+  );
+};
+export default App;
---
+++ services/services.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const apiUrl = 'https://api.themoviedb.org/3/';
+const apiKey = 'api_key=2a1a489250842c5ea5e346abcd83251f';
+
+// get Popular Movies
+export const getPopularMovies = async () => {
+  const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+  return resp.data.results;
+};
+
+// get Upcoming Movies
+export const getUpcomingMovies = async () => {
+  const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+  return resp.data.results;
+};
+
+// get Popular TV
+export const getPopularTv = async () => {
+  const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+  return resp.data.results;
+};
