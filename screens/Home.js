d80f22b14bb714eca8db63c4bb27b6b4ce7b15d6
Anthony Ruiz <yobakaj@gmail.com>
2023-05-22 08:52:59 -0400

    refactoring to call de services at the same time and get only one promisse and error, added an ActivityIndicator to show when dont get data from API

--- screens/Home.js
+++ screens/Home.js
@@ -1,56 +1,116 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Dimensions, ScrollView} from 'react-native';
-import {getPopularMovies, getUpcomingMovies} from '../services/services';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import {
+  getPopularMovies,
+  getUpcomingMovies,
+  getPopularTv,
+  getFamilyMovies,
+  getDocumentaryMovies,
+} from '../services/services';
 import {SliderBox} from 'react-native-image-slider-box';
 import List from '../components/List';
 
 const dimensions = Dimensions.get('screen');
 
 const Home = () => {
-  const [movieImages, setMovieImages] = useState('');
-  const [popularMovies, setPopularMovies] = useState('');
+  const [movieImages, setMoviesImages] = useState();
+  const [popularMovies, setPopularMovies] = useState();
+  const [familyMovies, setFamilyMovies] = useState();
+  const [documentaryMovies, setDocumentaryMovies] = useState();
+  const [popularTv, setPopularTv] = useState();
+
   const [error, setError] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  const getData = () => {
+    return Promise.all([
+      getUpcomingMovies(),
+      getPopularMovies(),
+      getFamilyMovies(),
+      getPopularTv(),
+      getDocumentaryMovies(),
+    ]);
+  };
 
   useEffect(() => {
-    getUpcomingMovies()
-      .then(movies => {
-        const moviesImagesArray = [];
-        movies.forEach(movie => {
-          moviesImagesArray.push(
-            'https://image.tmdb.org/t/p/original' + movie.poster_path,
-          );
-        });
-        setMovieImages(moviesImagesArray);
-      })
-      .catch(err => {
-        setError(err);
-      });
+    getData()
+      .then(
+        ([
+          upcomingMoviesData,
+          popularMoviesData,
+          popularTvData,
+          familyMoviesData,
+          documentaryMoviesData,
+        ]) => {
+          const moviesImagesArray = [];
+          upcomingMoviesData.forEach(movie => {
+            moviesImagesArray.push(
+              'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+            );
+          });
 
-    getPopularMovies()
-      .then(movies => {
-        setPopularMovies(movies);
-      })
+          setMoviesImages(moviesImagesArray);
+          setPopularMovies(popularMoviesData);
+          setPopularTv(popularTvData);
+          setFamilyMovies(familyMoviesData);
+          setDocumentaryMovies(documentaryMoviesData);
+        },
+      )
       .catch(err => {
         setError(err);
+      })
+      .finally(() => {
+        setLoaded(true);
       });
   }, []);
 
   return (
     <React.Fragment>
-      <ScrollView>
-        <View style={styles.sliderContainer}>
-          <SliderBox
-            images={movieImages}
-            autoplay={true}
-            circleLoop={true}
-            sliderBoxHeight={dimensions.height / 1.5}
-            dotStyle={styles.sliderStyle}
-          />
-        </View>
-        <View style={styles.carousel}>
-          <List title="Popular Movies" content={popularMovies} />
-        </View>
-      </ScrollView>
+      {loaded && (
+        <ScrollView>
+          {movieImages && (
+            <View style={styles.sliderContainer}>
+              <SliderBox
+                images={movieImages}
+                autoplay={true}
+                circleLoop={true}
+                sliderBoxHeight={dimensions.height / 1.5}
+                dotStyle={styles.sliderStyle}
+              />
+            </View>
+          )}
+          {popularMovies && (
+            <View style={styles.carousel}>
+              <List title="Popular Movies" content={popularMovies} />
+            </View>
+          )}
+
+          {familyMovies && (
+            <View style={styles.carousel}>
+              <List title="Family Movies" content={familyMovies} />
+            </View>
+          )}
+
+          {documentaryMovies && (
+            <View style={styles.carousel}>
+              <List title="Documentaries" content={documentaryMovies} />
+            </View>
+          )}
+
+          {popularTv && (
+            <View style={styles.carousel}>
+              <List title="Popular TV Shows" content={popularTv} />
+            </View>
+          )}
+        </ScrollView>
+      )}
+      {!loaded && <ActivityIndicator size="large" />}
     </React.Fragment>
   );
 };
