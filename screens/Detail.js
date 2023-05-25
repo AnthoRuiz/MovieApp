7f2c3faa69003978662b2c6fba4b00c2edf969c6
Anthony Ruiz <yobakaj@gmail.com>
2023-05-24 23:32:51 -0400

    added genres and start on detailMovie component, installed fontawesome on the project

--- screens/Detail.js
+++ screens/Detail.js
@@ -5,8 +5,11 @@ import {
   StyleSheet,
   Dimensions,
   ActivityIndicator,
+  Text,
+  View,
 } from 'react-native';
 import {getMovie} from '../services/services';
+import StarRating from 'react-native-star-rating';
 
 const placeholderImage = require('../assets/images/placeholder.png');
 const height = Dimensions.get('screen').height;
@@ -50,6 +53,27 @@ const Detail = ({route, navigation}) => {
                 : placeholderImage
             }
           />
+          <View style={styles.container}>
+            <Text style={styles.movieTitle}>{movieDetail.title}</Text>
+            {movieDetail.genres && (
+              <View style={styles.genreContainer}>
+                {movieDetail.genres.map(genre => {
+                  return (
+                    <Text style={styles.genreName} key={genre.id}>
+                      {genre.name}
+                    </Text>
+                  );
+                })}
+              </View>
+            )}
+            <StarRating
+              fullStarColor={'gold'}
+              starSize={30}
+              disabled={true}
+              maxStars={5}
+              rating={movieDetail.vote_average / 2}
+            />
+          </View>
         </ScrollView>
       )}
       {!loaded && <ActivityIndicator size="large" />}
@@ -58,8 +82,29 @@ const Detail = ({route, navigation}) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   image: {
     height: height / 2,
+  },
+  movieTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  genreContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  genreName: {
+    marginRight: 10,
+    fontWeight: 'bold',
   },
 });
 
